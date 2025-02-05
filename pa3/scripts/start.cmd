9b287704ce98f08d7ce6ba5023e52c1aeb198178
Mihail36 <mihailpetrov24@yandex.ru>
2025-02-05 15:07:30 +0300

    pa3

---
+++ pa3/scripts/start.cmd
@@ -0,0 +1,13 @@
+@echo off
+cd ..\nats-server\
+start nats-server.exe
+cd ..\RankCalculator\
+start dotnet run
+start dotnet run
+
+cd ..\Valuator\
+start dotnet run --urls "http://0.0.0.0:5001"
+start dotnet run --urls "http://0.0.0.0:5002"
+
+cd C:\Users\Misha\DP\DistributedProgramming\pa3\nginx
+start nginx -c C:\Users\Misha\DP\DistributedProgramming\pa3\nginx\conf\nginx.conf
